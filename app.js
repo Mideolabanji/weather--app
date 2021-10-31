ccce8594c5edceb3d012d2eabab535a82704c9ab
Mideolabanji <olabanjikaosara@yahoo.com>
2021-10-31 16:10:41 +0100

    updated the javascript

--- app.js
+++ app.js
@@ -59,7 +59,7 @@ function search(event) {
   }
   let apiKey = "21c25c62efe8c3f5cd46c74303b5daaf";
   let city = `${textInput.value}`;
-  let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayTemperature);
 }
 
